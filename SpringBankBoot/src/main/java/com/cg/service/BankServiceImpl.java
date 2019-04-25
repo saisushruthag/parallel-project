4c6c6907b7f23130245c462ad5460165322a75bb
saisushruthag <47023368+saisushruthag@users.noreply.github.com>
2019-04-25 18:21:33 +0530

    Add files via upload

---
+++ SpringBankBoot/src/main/java/com/cg/service/BankServiceImpl.java
@@ -0,0 +1,26 @@
+package com.cg.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cg.dao.BankDao;
+import com.cg.model.CustomerDetails;
+
+@Transactional
+@Service
+@Component("bank")
+public class BankServiceImpl implements BankService {
+
+	@Autowired
+	BankDao bankDao;
+	public CustomerDetails register(CustomerDetails cd) {
+		return bankDao.register(cd);
+	}
+	public int login(CustomerDetails c) {
+		return bankDao.login(c);
+	}
+
+
+}
